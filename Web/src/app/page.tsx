0e2da6e197f2817745eaeb5336f343a4def72fb0
Breno Martins Simão <brenomsimao123@outlook.com>
2023-05-15 22:37:46 -0300

    -> Concluded initial setup for web plataform

--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>Hello Word</h1>
+    <div className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold">
+      oi
+    </div>
   )
 }
