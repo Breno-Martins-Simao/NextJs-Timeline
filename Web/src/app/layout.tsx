3b8d9c8ec76759bda54536b3b3f5f9f636701d5f
Breno Martins Simão <brenomsimao123@outlook.com>
2023-05-16 19:28:02 -0300

    -> Configured use of new fonts for the project

--- Web/src/app/layout.tsx
+++ Web/src/app/layout.tsx
@@ -1,8 +1,20 @@
 import { ReactNode } from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamJuree,
+} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+})
+
+const baiJamJuree = BaiJamJuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-baijamjuree',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +24,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamJuree.variable} font-sans`}>
+        {children}
+      </body>
     </html>
   )
 }
--- Web/src/app/page.tsx
+++ Web/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-900 p-6">
       <h1 className="text-4xl font-bold">Your time Capsule</h1>
+      <h1 className="font-alt text-4xl font-bold">Your time Capsule</h1>
     </div>
   )
 }
