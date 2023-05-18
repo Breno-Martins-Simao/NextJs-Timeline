18cf984b5dd58b45549a9d1c00d98fed47c9a23d
Breno Martins Simão <brenomsimao123@outlook.com>
2023-05-17 21:09:38 -0300

    -> Concluded /register route

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { authRoutes } from './routes/auth'
 
 const port = 3333
@@ -9,6 +10,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true, // Change for Production url later
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 app.register(memoriesRoutes)
 app.register(authRoutes)
