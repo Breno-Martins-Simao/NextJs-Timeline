acae9322632afb2fce224a8e53cd06ec885cb882
Breno Martins Simão <brenomsimao123@outlook.com>
2023-05-18 20:43:29 -0300

    -> Redirect when no authenticated

--- Web/src/components/Hero.tsx
+++ Web/src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import spaceTimeLogo from '../assets/spacetime-logo.svg' // Logo import
 import Image from 'next/image'
 
@@ -15,12 +16,12 @@ export function Hero() {
         </p>
       </div>
 
-      <a
+      <Link
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-        href=""
+        href="/memories/new"
       >
         REGISTER A MEMORY
-      </a>
+      </Link>
     </div>
   )
 }
