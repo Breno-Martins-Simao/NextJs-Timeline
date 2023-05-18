f7b0011249521b33db3e0085cf9a5206281b4c36
Breno Martins Simão <brenomsimao123@outlook.com>
2023-05-18 19:49:47 -0300

    -> Created memory creating page and default layout

--- Web/src/app/layout.tsx
+++ Web/src/app/layout.tsx
@@ -4,6 +4,11 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamJuree,
 } from 'next/font/google'
+import { Profile } from '@/components/Profile'
+import { SignIn } from '@/components/SignIn'
+import { Hero } from '@/components/Hero'
+import { Copyright } from '@/components/Copyright'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -23,12 +28,30 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamJuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Grid Left Section */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full " />
+            {/* Stripe */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+            {/* Components */}
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+          {/* Grid Right Section */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {/* Adaptative Content */}
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
