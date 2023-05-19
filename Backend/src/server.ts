f12d23e738fb73a332fd6db3ab2bd68a6848724f
Breno Martins Simão <brenomsimao123@outlook.com>
2023-05-18 23:44:55 -0300

    -> Added upload files routes and static service

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -3,7 +3,12 @@ import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
+
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const port = 3333
 const app = fastify()
@@ -12,11 +17,20 @@ app.register(cors, {
   origin: true, // Change for Production url later
 })
 
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(multipart) // File Stream
+
 app.register(jwt, {
   secret: 'spacetime',
-})
-app.register(memoriesRoutes)
+}) // JWT usage
+
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
