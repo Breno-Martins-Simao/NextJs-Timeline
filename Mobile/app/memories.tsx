245c3edb9512f43a6d4986c2a47780058789ae0a
Breno Martins Simão <brenomsimao123@outlook.com>
2023-05-19 22:35:05 -0300

    >Reordened pages and change animations transitions

--- Mobile/app/memories.tsx
+++ Mobile/app/memories.tsx
@@ -1,9 +1,30 @@
-import { View, Text } from 'react-native'
+import { TouchableOpacity, View, ScrollView } from 'react-native'
+import { Link } from 'expo-router'
 
-export default function Memories() {
+// Images & Icons
+import AppLogo from '../src/assets/nlw-spacetime-logo.svg'
+import Icon from '@expo/vector-icons/Feather'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+// Secure Store
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <AppLogo />
+
+        <Link href="/new" asChild>
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+            <Icon name="plus" size={16} color="#000" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </ScrollView>
   )
 }
